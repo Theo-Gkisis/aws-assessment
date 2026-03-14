b862ecb70493933db83251461cf0c4ba58817a4e
TheodoreGisis <teo-otenet@hotmail.com>
2026-03-14 20:29:41 +0200

    add right repo url

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   project_name = "unleash-live-assessment"
   email        = "theodorosgkisis@gmail.com"
   github_name  = "theodorosgkisis"
-  github_repo  = "https://github.com/Theo-Gkisis/aws-assesment"
+  github_repo  = "https://github.com/Theo-Gkisis/aws-assessment"
   common_tags = {
     Project   = "unleash-live-assessment"
     Email     = "theodorosgkisis@gmail.com"
